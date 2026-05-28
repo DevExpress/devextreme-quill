19d00b4fce067127ca35c2806bfbcc58db7e0f62
Andrey Vorobev <dobriy.kaa@gmail.com>
2026-05-28 10:30:23 +0300

    Code scanning Polynomial regexp fix (#233)
    * Code scanning Polynomial regexp fix
    
    * fix regex
    
    * Quill restrict spaces for ten max for regexp
    
    ---------
    
    Co-authored-by: Andrei Vorobev <andrei.vorobev@devexpress.com>

--- core/quill.js
+++ core/quill.js
@@ -472,10 +472,10 @@ Quill.version = typeof QUILL_VERSION === 'undefined' ? 'dev' : QUILL_VERSION;
 Quill.MS_LIST_DATA_KEY = 'mso-list-data';
 
 Quill.replaceStyleAttribute = (html) => {
-  const tagAttrsRegex = /(?:(<[a-z0-9]+\s*))([\s\S]*?)(>|\/>)/gi;
+  const tagAttrsRegex = /(<[a-z0-9]+)([^>]*)(>)/gi;
 
   return html.replace(tagAttrsRegex, (allTagAttrs, tagStart, tagAttrs, tagEnd) => {
-    const contentWithoutStyle = tagAttrs.replace(/style\s*=/gi, `${STYLE_ATTRIBUTE_KEY}=`);
+    const contentWithoutStyle = tagAttrs.replace(/style\s{0,10}=/gi, `${STYLE_ATTRIBUTE_KEY}=`);
 
     return tagStart + contentWithoutStyle + tagEnd;
   });
